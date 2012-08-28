e7f9fb29e5b611eda5004722f7a50611d0459685
Barry Clark <bazclef@gmail.com>
2012-08-28 17:20:01 -0400

    Another test commit

--- header.php
+++ header.php
@@ -2,7 +2,7 @@
 <html <?php language_attributes(); ?>>
 <head>
 
-<!-- Meta Tags & Browser stuff -->
+<!-- Meta tags & browser stuff -->
 <meta charset="<?php bloginfo('charset'); ?>" />
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
