6bb3ce1ca000ca3f6cbdb90ec1276d1cf91bf491
Barry Clark <bazclef@gmail.com>
2012-08-30 00:49:21 -0400

    Added back in Google Analytics hook

--- header.php
+++ header.php
@@ -35,7 +35,19 @@
 <link href='http://fonts.googleapis.com/css?family=Varela+Round' rel='stylesheet' type='text/css'/>
 
 <!-- Google Analytics -->
-    
+<?php $slim_ga_tracking_code = get_option('slim_ga_tracking_code'); if ($slim_ga_tracking_code) : ?>
+<script type="text/javascript">
+	var _gaq = _gaq || [];
+	_gaq.push(['_setAccount', '<?php echo $slim_ga_tracking_code; ?>']);
+	_gaq.push(['_trackPageview']);
+	(function() {
+		var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+		ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+		var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+	})();
+</script>
+<?php endif; ?>
+
 </head>
 
 <body <?php body_class(); ?>>
