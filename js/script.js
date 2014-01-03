51f8e4849189b899d2aba79e67625a43a0e50a58
Barry Clark <barry@barryclark.co>
2014-01-02 21:53:39 -0500

    cleanup

--- js/script.js
+++
@@ -1,7 +0,0 @@
-// HIDE MENU BAR WHEN PAGE LOADS (iOS)
-addEventListener('load', function() { 
-	setTimeout(hideAddressBar, 0); 
-}, false);
-function hideAddressBar() { 
-	window.scrollTo(0, 1); 
-}
